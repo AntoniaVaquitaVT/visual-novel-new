e09ec60a95a524acbddad25acf0066d15b796dde
Rocio Diaz <rdiaz@cfotech.com>
2021-10-10 18:05:25 -0300

    basic structure and installs

---
+++ src/redux/listingsReducer.js
@@ -0,0 +1,19 @@
+import listingsService from '../services/listingsService';
+const listingsReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'INIT_LISTINGS':
+            return action.data;
+        default:
+            return state;
+    }
+};
+export const initListings = () => {
+    return async dispatch => {
+       const listings = await listingsService.getAll(); 
+       dispatch({
+           type: 'INIT_LISTINGS',
+           data: listings,
+       });
+    };
+};
+export default listingsReducer;
